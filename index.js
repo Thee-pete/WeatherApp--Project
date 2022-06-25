0455e75c68328a5103694a06e5ace683252326b5
Peter Ndumia <ndegwandumia@gmail.com>
2022-06-25 15:17:15 +0300

    Created local server

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed');
     getWeather();
+    loadFavourites();
+    addToFavorites();
+  
 });
 
 let key = 'e7d25d8c1d14be6b058717f22a1b77ea';
 let city = 'Cape Town';
 let units = 'Metric';
 let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=${units}`;
+let favList = document.getElementById('favoritesList');
+console.log(favList);
 
 function getWeather(){
     fetch(url)
@@ -27,8 +31,8 @@ function displayWeather(details){
 
     document.getElementById('cityName').textContent = details.name;
     document.getElementById('weatherIcon').src = `http://openweathermap.org/img/wn/${details.weather[0].icon}.png`;
-    document.getElementById('weatherMain').textContent = details.weather[0].main;
-    document.getElementById('weatherDescription').textContent = details.weather[0].description;
+    document.getElementById('weatherMain').textContent = `Weather: ${details.weather[0].main}`;
+    document.getElementById('weatherDescription').textContent = `Description: ${details.weather[0].description}`;
     document.getElementById('humidity').textContent = `Humidity: ${details.main.humidity}`;
     document.getElementById('temperature').textContent = `Temperature: ${details.main.temp}`;
     document.getElementById('pressure').textContent = `Pressure: ${details.main.pressure}`;
@@ -38,7 +42,45 @@ function displayWeather(details){
 
     console.log(details.name)
 
-
-
 }
 
+function addToFavorites(){
+   
+
+    document.getElementById('favoriteBtn').addEventListener('click' ,() => {
+        //add city name to favorites list
+        let favouriteName = document.getElementById('cityName');
+        let newFav = document.createElement('li');
+        newFav.appendChild(document.createTextNode(favouriteName.textContent));
+        favList.appendChild(newFav);
+
+        let newCityFav = newFav.textContent;
+
+        fetch('http://localhost:3000/favorites', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                cityName: `${newCityFav}`,
+            } ),
+        });
+
+       });
+}
+function loadFavourites(){
+    fetch('http://localhost:3000/favorites')
+    .then(resp => resp.json())
+    .then(data => {
+
+      
+       for(let i = 0; i < data.length; i++)
+       {
+        let fav = document.createElement('li')
+        fav.appendChild(document.createTextNode(data[i].cityName));
+        favList.appendChild(fav);
+        
+       }
+
+    })
+}
