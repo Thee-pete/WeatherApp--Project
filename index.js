03790fcce5626faea949ddde01c3d720b868e01e
Peter Ndumia <ndegwandumia@gmail.com>
2022-06-25 13:00:11 +0300

    All fields fetched from API

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 let key = 'e7d25d8c1d14be6b058717f22a1b77ea';
 let city = 'Nairobi';
-let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
+let units = 'Metric';
+let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=${units}`;
 
 function getWeather(){
     fetch(url)
@@ -18,6 +19,26 @@ function getWeather(){
     })
     .then(data => {
         console.log(data)
+        displayWeather(data);
     })
+    .catch(error => console.log(error));
+}
+function displayWeather(details){
+
+    document.getElementById('cityName').textContent = details.name;
+    document.getElementById('weatherIcon').src = `http://openweathermap.org/img/wn/${details.weather[0].icon}.png`;
+    document.getElementById('weatherMain').textContent = details.weather[0].main;
+    document.getElementById('weatherDescription').textContent = details.weather[0].description;
+    document.getElementById('humidity').textContent = `Humidity: ${details.main.humidity}`;
+    document.getElementById('temperature').textContent = `Temperature: ${details.main.temp}`;
+    document.getElementById('pressure').textContent = `Pressure: ${details.main.pressure}`;
+    document.getElementById('temp_max').textContent = `Max temperature: ${details.main.temp_max}`;
+    document.getElementById('temp_min').textContent = `Min temperature: ${details.main.temp_min}`;
+    
+
+    console.log(details.name)
+
+
+
 }
 
